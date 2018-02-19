be174ff5155be11c0eecec0ba6d2f34e5d51c161
rodrigo.torres <rodrigo.torres@vagas.com.br>
2018-02-19 14:44:41 -0300

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,9 @@ description 'Installs/Configures redirects'
 long_description 'Installs/Configures redirects'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+issues_url 'https://github.com/rlinstorres/redirects/issues'
+source_url 'https://github.com/rlinstorres/redirects'
+supports 'ubuntu', '>=14.04'
+license 'Apache-2.0'
 
 depends 'apache2', '~> 5.0.1'
